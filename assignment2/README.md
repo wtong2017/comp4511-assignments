ef78383cee5e002f940c95a29a27682f686aade6
wtong2017 <wtong@connect.ust.hk>
2018-02-27 20:29:25 +0800

    Run program using path env var

--- assignment2/README.md
+++ assignment2/README.md
@@ -2,3 +2,4 @@
 - [x] refactor code 
 - [x] read the cmdline into a 2d char array
 - [ ] apply different functions according to the 2d char array
+- [ ] print the correct output format for feature 4
--- assignment2/myshell.c
+++ assignment2/myshell.c
@@ -6,12 +6,14 @@
 #include <sys/types.h>
 
 #define MAX_CMDLINE_LEN 256
+#define MAX_PROGRAM_PATH 128
 
 /* function prototypes go here... */
 void show_prompt();
 int get_cmd_line(char *cmdline);
 void process_cmd(char *cmdline);
-int tokenize_cmd_line(char tokens[MAX_CMDLINE_LEN][MAX_CMDLINE_LEN], char *cmdline);
+int tokenize_cmd_line(char *tokens[MAX_CMDLINE_LEN], char *cmdline);
+int get_program_path(char *paths[MAX_PROGRAM_PATH]);
 void run_program(const char *filename, char *const argv[], char *const envp[]);
 void my_cd(char *path);
 void child_command();
@@ -35,27 +37,32 @@ int main()
 void process_cmd(char *cmdline)
 {
 	// printf("%s\n", cmdline);
-	char tokens[MAX_CMDLINE_LEN][MAX_CMDLINE_LEN];
+	char *tokens[MAX_CMDLINE_LEN];
 	int token_num = tokenize_cmd_line(tokens, cmdline);
+	// Exit
 	if (token_num == 1 && strncmp(tokens[0], "exit", 4) == 0) {
 		printf("myshell is terminated with pid %i\n", getpid());
 		exit(0);
 	}
+	// Child cmd from lab
 	if (token_num == 2 && strncmp(tokens[0], "child", 5) == 0) {
 		if (str_to_int(tokens[1]) == 1) {
 			child_command(atoi(tokens[1]));
 			return;
 		}
 	}
-	if (token_num == 1 && strncmp(tokens[0], "cd", 2) == 0) {
-		my_cd(NULL);
-		return;
-	}
-	if (token_num == 2 && strncmp(tokens[0], "cd", 2) == 0) {
+	//if (token_num == 1 && strncmp(tokens[0], "cd", 2) == 0) {
+	//	my_cd(NULL);
+	//	return;
+	//}
+	// My cd cmd using chdir
+	if ((token_num == 1 || token_num == 2) && strncmp(tokens[0], "cd", 2) == 0) {
 		my_cd(tokens[1]);
 		return;
 	}
-	printf("Invalid command\n");
+	// Other cmd
+	run_program(tokens[0], tokens, NULL);
+	//printf("Invalid command\n");
 }
 
 void show_prompt() 
@@ -96,29 +103,58 @@ int get_cmd_line(char *cmdline)
     return 0;
 }
 
-int tokenize_cmd_line(char tokens[MAX_CMDLINE_LEN][MAX_CMDLINE_LEN], char *cmdline) {
-	int count = 0;
-	int i, l;
-	char token[MAX_CMDLINE_LEN];
-	for (i = 0; 1 == sscanf(cmdline + i, "%s%n", token, &l); i = i + l) {
-		strcpy(tokens[count], token);
-		count += 1;
+int tokenize_cmd_line(char *tokens[MAX_CMDLINE_LEN], char *cmdline) {
+	int i = 0;
+	char *pch = strtok(cmdline, " \t");
+	tokens[i] = pch;
+	while (pch != NULL) {
+		//printf("%s\n", pch);
+		pch = strtok(NULL, " \t");
+		i++;
+		tokens[i] = pch;
 	}
-	//tokens[count] = NULL;
- 	return count;
+ 	return i;
 }
 
 void run_program(const char *filename, char *const argv[], char *const envp[]) {
-
+	pid_t pid = fork();
+	if (pid == 0) { // Child
+		//printf(filename);
+		//if (strncmp(filename, "cd", 2) == 0) {
+		//	my_cd(argv[1]);
+		//}
+		char *paths[MAX_PROGRAM_PATH];
+		int path_num = get_program_path(paths);
+		int i;
+		char path_buffer[512];
+		strcpy(path_buffer, "./"); // current directory
+		strcat(path_buffer, filename);
+		if (-1 == (execve(path_buffer, argv, NULL))) {
+			for (i = 0; i < path_num; i++) {
+				strcpy(path_buffer, paths[i]);
+				strcat(path_buffer, "/");
+				strcat(path_buffer, filename);
+				//printf("%s\n", path_buffer);
+				if (-1 == (execve(path_buffer, argv, NULL)) && i == path_num-1) {
+					perror("execve");  
+        				exit(-1);
+				}
+			}
+    		}
+		exit(0);
+	}
+	// Parent
+	wait(0);
 }
 
 void my_cd(char *path) {
 	if (!path) {
 		path = getenv("HOME");	
 	}
-	// printf("Path: %s\n", path);	
+	//printf("Path: %s\n", path);	
 	if (chdir(path) == -1) {
-		printf("Encounter errors\n");
+		perror("chdir");
+		//exit(-1);
 	}
 }
 
@@ -143,4 +179,18 @@ int str_to_int(char *str) {
    		str++;
 	}
 	return 1;
+}
+
+int get_program_path(char *paths[MAX_PROGRAM_PATH]) {
+	char* path_var = getenv("PATH");
+	int i = 0;
+	char *pch = strtok(path_var, ":");
+	paths[i] = pch;
+	while (pch != NULL) {
+		//printf("%s\n", pch);
+		pch = strtok(NULL, ":");
+		i++;
+		paths[i] = pch;
+	}
+ 	return i;
 }
